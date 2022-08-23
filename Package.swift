d8705feb3b39fa65c16b1622a279839a974d3c36
foray-austin <101315215+foray-austin@users.noreply.github.com>
2022-08-22 23:45:29 -0700

    fix 2

--- Package.swift
+++ Package.swift
@@ -1,4 +1,18 @@
-// swift-tools-version:5.0
+// swift-tools-version:4.2
 import PackageDescription
 
-let package = Package(name: "IBLocalizable")
+let package = Package(
+    name: "IBLocalizable",
+    products: [
+        .library(name: "IBLocalizable", targets: ["IBLocalizable"])
+    ],
+    dependencies: [
+        .Package(url: "https://github.com/PiXeL16/IBLocalizable.git",majorVersion: 1)
+    ],
+    targets: [
+        .target(
+            name: "IBLocalizable",
+            path: "IBLocalizable"
+        )
+    ]
+)
